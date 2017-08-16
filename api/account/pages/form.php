dba22943b9b7e0d43c8ca787a0edaddce8c75609
PikachuVina <bmn.2312@gmail.com>
2017-08-16 08:27:14 +0700

    1

--- api/account/pages/form.php
+++
@@ -1,5 +0,0 @@
-﻿<form action="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">
-	<label>Name:</label>
-	<input name="name" placeholder="Mạnh Nghĩa">
-	<input type="submit" value="Tạo">
-</form>
