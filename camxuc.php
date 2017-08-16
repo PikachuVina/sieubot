8252e3988c6d5fd5d301233649ae003db3f142a5
PikachuVina <bmn.2312@gmail.com>
2017-08-16 16:10:24 +0700

    1

--- camxuc.php
+++ camxuc.php
@@ -10,19 +10,19 @@ if ($_POST && $_SESSION[id])
 	$token = $_SESSION['token'];
 	if (!$yeucau) 
 	{
-		die('ERROR: Không Tìm Thấy Yêu Cầu.');
+		echo('ERROR: Không Tìm Thấy Yêu Cầu.');
 	}
 	$loai = $_POST['auto'];
 	if ($loai == "botcamxuc"){
 		$tk = checktk($token);
 		if($tk['id'] !== '6628568379'){
-                die '<h4>ERROR: Đăng Nhập Bằng Tài Khoản FB và Chọn Apps là FACEBOOK FOR IPHONE Để Sử Dụng Chức Năng Này</h4>';
+                echo '<h4>ERROR: Đăng Nhập Bằng Tài Khoản FB và Chọn Apps là FACEBOOK FOR IPHONE Để Sử Dụng Chức Năng Này</h4>';
                 }
 	        $camxuc =  mysqli_real_escape_string($GLOBALS["___BMN_2312"], $_POST['camxuc']);
 		if($yeucau == "OK"){
 			$res = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM botcamxuc WHERE user_id = $idfb");
 			if (mysqli_num_rows($res) > 0) {
-				die('ERROR: Bạn Đang Sử Dụng BOT Trên Hệ Thống Của Chúng Tôi.');
+				echo('ERROR: Bạn Đang Sử Dụng BOT Trên Hệ Thống Của Chúng Tôi.');
 			}
 			@mysqli_query($GLOBALS["___BMN_2312"], "INSERT INTO botcamxuc SET 
 				`user_id` = '".$idfb."', 
@@ -31,11 +31,11 @@ if ($_POST && $_SESSION[id])
 				`usercai` = '" . mysqli_real_escape_string($GLOBALS["___BMN_2312"], $_SESSION['id']) . "',
 				`name` = '" . mysqli_real_escape_string($GLOBALS["___BMN_2312"], $_SESSION['name']) . "'
 				");
-			die('SUCCESS: Cài Đặt BOT Thành Công. BOT Sẽ Hoạt Động Từ 5-10 Phút Tới.<meta http-equiv="refresh" content="3">');
+			echo('SUCCESS: Cài Đặt BOT Thành Công. BOT Sẽ Hoạt Động Từ 5-10 Phút Tới.');
 		}elseif ($yeucau == "UP") {
 			$res = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM botcamxuc WHERE user_id = $idfb");
 			if (mysqli_num_rows($res) <= 0) {
-				die('ERROR: Bạn Không Sử Dụng BOT Trên Hệ Thống Của Chúng Tôi. Chọn ON Để Tiến Hành Cài Đặt BOT');
+				echo('ERROR: Bạn Không Sử Dụng BOT Trên Hệ Thống Của Chúng Tôi. Chọn ON Để Tiến Hành Cài Đặt BOT');
 			}
 			@mysqli_query($GLOBALS["___BMN_2312"], "UPDATE botcamxuc
 				         SET
@@ -45,22 +45,22 @@ if ($_POST && $_SESSION[id])
 				         WHERE
 				            `user_id` = ".$idfb."
 				      ");
-			die('SUCCESS: Cập Nhật BOT Thành Công. BOT Sẽ Hoạt Động Từ 5-10 Phút Tới.<meta http-equiv="refresh" content="3">');
+			echo('SUCCESS: Cập Nhật BOT Thành Công. BOT Sẽ Hoạt Động Từ 5-10 Phút Tới.');
 		}elseif ($yeucau == "HUY") 
 		{
 			$res = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM botcamxuc WHERE user_id = $idfb");
 			if (mysqli_num_rows($res) <= 0) {
-				die('ERROR: Bạn Không Sử Dụng BOT Trên Hệ Thống Của Chúng Tôi. Chọn ON Để Tiến Hành Cài Đặt BOT');
+				echo('ERROR: Bạn Không Sử Dụng BOT Trên Hệ Thống Của Chúng Tôi. Chọn ON Để Tiến Hành Cài Đặt BOT');
 			}
 			@mysqli_query($GLOBALS["___BMN_2312"], "DELETE FROM botcamxuc
 			            WHERE
 			               user_id = '".$idfb."'
 			         ");
-			die('SUCCESS: Xóa BOT Thành Công.<meta http-equiv="refresh" content="3">');
+			echo('SUCCESS: Xóa BOT Thành Công.');
 		}
 		else
 		{
-			die('ERROR: Sai Cú Pháp.');
+			echo('ERROR: Sai Cú Pháp.');
 		}
 	}
 		
