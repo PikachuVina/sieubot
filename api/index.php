93256a250e9c369d4ac2799b0e7af27e6e1c7695
PikachuVina <bmn.2312@gmail.com>
2017-08-16 08:13:12 +0700

    1

--- api/index.php
+++ api/index.php
@@ -13,6 +13,7 @@ include '../system/head.php';
   <a class="list-group-item" href="/api/getdrive.php"><i class="fa fa-rocket"></i> Get Link Trực Tiếp Google Drive </a>
   <a class="list-group-item" href="/api/getonbox.php"><i class="fa fa-rocket"></i> Get Link Max Băng Thông Onbox </a>
   <a class="list-group-item" href="/api/upanh.php"><i class="fa fa-rocket"></i> Upload Ảnh Trực Tuyến Imgur </a>
+  <a class="list-group-item" href="/api/proxy"><i class="fa fa-rocket"></i> Proxy US Đăng Nhập Ẩn Danh </a>
   <a class="list-group-item" href="#"><i class="fa fa-rocket"></i> Đang Cập Nhật Tính Năng ... </a>
   <a class="list-group-item" href="#"> Powered by <b>Bùi Mạnh Nghĩa</b> - <b>Nghĩa.Vn</b></a>
   </div>
