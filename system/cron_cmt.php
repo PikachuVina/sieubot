34e2c1c34b8b08619872e5efe0b7fe9256e55861
PikachuVina <bmn.2312@gmail.com>
2017-08-18 15:15:50 +0700

    1

--- system/cron_cmt.php
+++ system/cron_cmt.php
@@ -1,8 +1,8 @@
 <?php 
 error_reporting(0);
 set_time_limit(0); 
-ob_start('ob_gzhandler'); 
-require('config.php'); 
+
+include('config.php'); 
 $gettoken = mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `botcomment` ORDER BY RAND() LIMIT 0,4"); 
 while ($post = mysqli_fetch_array($gettoken)){ 
 $matoken= $post['access_token']; 
@@ -34,7 +34,7 @@ $b=count($stat['data']);
     if (strpos($time, $timelocpost) !== false) {
 		/* Check stt */
             if (strpos($logpost, $idpost) === FALSE) {
-				auto('https://graph.facebook.com/'.$stat['data'][$i-1]['id'].'/comments?message='.urlencode($message).'&access_token='.$token.'&method=post');
+				request('https://graph.facebook.com/'.$stat['data'][$i]['id'].'/comments?message='.urlencode($message).'&access_token='.$token.'&method=post');
                 $luulog = fopen("logCMT.txt", "a");
                 fwrite($luulog, $idpost . "\n");
                 fclose($luulog);
@@ -49,4 +49,34 @@ $b=count($stat['data']);
     }
 
 } 
+
+function request($url)
+{
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+        return FALSE;
+    }
+    
+    $options = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_HEADER => FALSE,
+        CURLOPT_FOLLOWLOCATION => TRUE,
+        CURLOPT_ENCODING => '',
+        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
+        CURLOPT_AUTOREFERER => TRUE,
+        CURLOPT_CONNECTTIMEOUT => 15,
+        CURLOPT_TIMEOUT => 15,
+        CURLOPT_MAXREDIRS => 5,
+        CURLOPT_SSL_VERIFYHOST => 2,
+        CURLOPT_SSL_VERIFYPEER => 0
+    );
+    
+    $ch = curl_init();
+    curl_setopt_array($ch, $options);
+    $response  = curl_exec($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    unset($options);
+    return $http_code === 200 ? $response : FALSE;
+}
 ?> 
