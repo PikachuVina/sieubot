2500c7b46fc78671f16f7127dc47c825372ff9dd
PikachuVina <bmn.2312@gmail.com>
2017-08-18 14:18:38 +0700

    1

--- system/Cron.php
+++ system/Cron.php
@@ -36,16 +36,37 @@ $com = json_decode(auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/co
     
 if($_GET['hanhdong'] == 'cmt')
 {
+$timelocpost = date('Y-m');
+$logpost= file_get_contents("logCMT.txt");
+
 $res = mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `botcomment` ORDER BY RAND() LIMIT 0,7");
 while ($post = mysqli_fetch_array($res)){
 $token= $post['access_token'];
+$check = json_decode(auto('https://graph.facebook.com/me?access_token='.$token),true); 
+if(!$check['id']){ 
+@mysql_query("DELETE FROM botcomment WHERE access_token ='".$token."'"); 
+continue; 
+}
 $message = $post['noidung'];
-$stat = json_decode(auto('https://graph.beta.facebook.com/me/home?fields=id,from&limit=10&access_token='.$token),true);
+$stat = json_decode(auto('https://graph.beta.facebook.com/me/home?fields=id,created_time,from&limit=10&access_token='.$token),true);
 $b=count($stat[data]);
     for($i=0; $i<=$b; $i++){
+    $idpost      = $post['data'][$i]['id'];
+    $time        = $post['data'][$i]['created_time'];
+    if (strpos($time, $timelocpost) !== false) {
+		/* Check stt */
+            if (strpos($logpost, $idpost) === FALSE) {
     // Hành Động...
 	auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/comments?message='.urlencode($message).'&access_token='.$token.'&method=post');
-}}
+                $luulog = fopen("logCMT.txt", "a");
+                fwrite($luulog, $idpost . "\n");
+                fclose($luulog);
+            } else {
+                echo 'Đã comment status này rồi';
+            }
+		/* END Check stt */
+    }
+	}}
 }
 
 
