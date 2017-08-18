5c88d0ee4e619438063ac33600907b82aaeaa8a8
PikachuVina <bmn.2312@gmail.com>
2017-08-18 15:10:11 +0700

    1

--- system/cron_cmt.php
+++ system/cron_cmt.php
@@ -2,39 +2,51 @@
 error_reporting(0);
 set_time_limit(0); 
 ob_start('ob_gzhandler'); 
-include'config.php';
+require('config.php'); 
 $gettoken = mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `botcomment` ORDER BY RAND() LIMIT 0,4"); 
-while ($row = mysqli_fetch_array($gettoken)){ 
-$token= $row['access_token']; 
-$check = json_decode(file_get_contents('https://graph.facebook.com/me?access_token='.$token),true); 
+while ($post = mysqli_fetch_array($gettoken)){ 
+$matoken= $post['access_token']; 
+$check = json_decode(file_get_contents('https://graph.facebook.com/me?access_token='.$matoken),true); 
 if(!$check['id']){ 
 @mysqli_query($GLOBALS["___BMN_2312"], "DELETE FROM botcomment 
             WHERE 
-               access_token ='".$token."' 
+               access_token ='".$matoken."' 
          "); 
 continue; 
 } 
+
+
+$result = mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `botcomment` ORDER BY RAND() LIMIT 0,4"); 
 $timelocpost = date('Y-m');
 $logpost= file_get_contents("logCMT.txt");
-$message = $row['noidung'];
-$stat = json_decode(auto('https://graph.beta.facebook.com/me/home?fields=id,created_time,from&limit=1&access_token='.$token),true);
-$b=count($stat[data]);
+
+   if($result){ 
+       while($row = mysqli_fetch_array($result)){ 
+$message =  $row['noidung'];
+$token = $row['access_token']; 
+$stat = json_decode(auto('https://graph.facebook.com/v2.9/me/home?fields=id,created_time,from&limit=1&access_token=' . $token), true); /* Get Data Post*/
+  
+$b=count($stat['data']);
     for($i=0; $i<=$b; $i++){
     $idpost      = $stat['data'][$i]['id'];
     $time        = $stat['data'][$i]['created_time'];
+	
     if (strpos($time, $timelocpost) !== false) {
-    /* Check stt */
-    if (strpos($logpost, $idpost) === FALSE) {
-    // Hành Động...
-	auto('https://graph.facebook.com/'.$stat['data'][$i-1]['id'].'/comments?message='.urlencode($message).'&access_token='.$token.'&method=post');
-	$luulog = fopen("logCMT.txt", "a");
-		fwrite($luulog, $idpost . "\n");
-		fclose($luulog);
-		} else {
-		echo 'Đã comment status này rồi';
-		}
-	/* END Check stt */
+		/* Check stt */
+            if (strpos($logpost, $idpost) === FALSE) {
+				auto('https://graph.facebook.com/'.$stat['data'][$i-1]['id'].'/comments?message='.urlencode($message).'&access_token='.$token.'&method=post');
+                $luulog = fopen("logCMT.txt", "a");
+                fwrite($luulog, $idpost . "\n");
+                fclose($luulog);
+            } else {
+                echo 'Đã comment status này rồi';
+            }
+		/* END Check stt */
     }
-	}
 }
-?>
+				
+        } 
+    }
+
+} 
+?> 
